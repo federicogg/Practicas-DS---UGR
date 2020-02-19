6ae0cb95bfda7daaecd9d349d87ceedd06e82861
fede <fede@fede-UX390UAK>
2020-02-19 13:11:06 +0100

    commit

--- src/practicasDSD/FactoriaBicicletasMontana.java
+++ src/practicasDSD/FactoriaBicicletasMontana.java
@@ -2,12 +2,10 @@ package practicasDSD;
 
 public class FactoriaBicicletasMontana implements FactoriaAbstracta{
 	
-<<<<<<< Upstream, based on origin/master
-=======
 	public Bicicleta crearBicicleta()
 	{
 		BicicletaMontana bicicletaMontana = new BicicletaMontana();
 		return bicicletaMontana;
 	}
->>>>>>> 74cebb1 commit
+
 }
