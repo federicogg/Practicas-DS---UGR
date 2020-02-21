f7049f67a5f85a7b838f6bd6eb12c9d128669bb0
fede <fede@fede-UX390UAK>
2020-02-21 03:19:02 +0100

    commit

--- src/practicasDSD/Carrera.java
+++ src/practicasDSD/Carrera.java
@@ -1,20 +1,32 @@
 package practicasDSD;
 
-import java.beans.IntrospectionException;
+
 import java.util.ArrayList;
 
-public class Carrera
+public class Carrera extends Thread
 {
 	public ArrayList <Bicicleta> bicicletas;
 	protected Tipo tipo;
 	
-	
+	public void addBicicletas (ArrayList<Bicicleta> bicicletas)
+	{
+		this.bicicletas = bicicletas;
+	}
 	
 	public Carrera(Tipo tipo) 
 	{
 		this.tipo = tipo;
+	}
+	
+	
+	@Override
+	public void run()
+	{
 		
 	}
+	
+	
+	
 	
 		
 }
--- src/practicasDSD/Cliente.java
+++ src/practicasDSD/Cliente.java
@@ -1,13 +1,48 @@
 package practicasDSD;
 
+import javax.swing.*;
+
+import java.awt.BorderLayout;
+import java.util.Random;
+
+
+
+
 public class Cliente 
 {
 	
 	
-	public static int main (String[] args)
+	
+	public static void main (String[] args)
 	{
+		//Creamos dos factorias cada uno de un tipo diferente de subproducto
+		FactoriaAbstracta factoriaCarretera = new FactoriaCarretera();
+		FactoriaAbstracta factoriaMontana = new FactoriaMontana();
+	 
+		//Preparamos la carrera
+		//Creamos un número aleatorio N (entre 10 y 40) para crear el número de bicicletas en las carreras
+		Integer n = getRandomNumberInRange(10, 40);
 		
 		
-		return 0;
+			
+	
+		
+		
+		
+		
+		
+		
+
+	}
+	
+	
+	private static int getRandomNumberInRange(int min, int max) {
+
+		if (min >= max) {
+			throw new IllegalArgumentException("max must be greater than min");
+		}
+
+		Random r = new Random();
+		return r.nextInt((max - min) + 1) + min;
 	}
 }
